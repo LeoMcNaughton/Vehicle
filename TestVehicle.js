6fafcf729ba45ba56b39550d541f29743811d32e
Leonardo McNaughton <9lmcnaughton@stpatrick.org>
2017-09-01 14:39:01 -0500

    will not compile for some reason

--- TestVehicle.js
+++ TestVehicle.js
@@ -2,11 +2,21 @@ const Vehicle = require('./Vehicle.js');
 
 function main(){
     //make two vehicles and take them for a spin.
-   let flemborgini
+   let flemborgini = new Vehicle(8,1,1,30,300);
+   console.log(flemborgini.fuelLeft());
+   flemborgini.drive(2);
+   console.log(flemborgini.fuelLeft());
+   console.log(flemborgini.totalMiles());
+   flemborgini.fill(0.5);
+   console.log(flemborgini.fill());
 
-
-
-
+   let flemrrai= new Vehicle(8,1,1,25,250);
+   console.log(flemrrai.fuelLeft());
+   flemrrai.drive(3);
+   console.log(flemrrai.fuelLeft());
+   console.log(flemrrai.totalMiles());
+   flemborgini.fill(2);
+   console.log(flemrrai.fill());
 }
 //run the main method
 main();
--- Vehicle.js
+++ Vehicle.js
@@ -1,1 +1,50 @@
-  let vehicle
+let Vehicle = function(e,f,gt,aMPG,mD){
+  let engine = e;
+  let fuel = f;
+  let gastank = gt;
+  let averageMPG = aMPG;
+  let milesDriven = mD;
+
+  function totalMiles(){
+    return milesDriven;
+  }
+  function fuelLeft(){
+    return fuel;
+  }
+  function fill(){
+    let gastank = 0;
+    fuel += p;
+
+    if(f => 1){
+      fuel = 1;
+    }
+    }
+  }
+  function drive(){
+    let fuelburned = (engineEfficiency()*hours)
+    if(fuelburned <= fuel){
+      milesDriven += (hours*aMPG);
+      fuel -= (engineEfficiency()*hours);
+    }
+    else{
+      milesDriven = fuel*aMPG;
+      fuel = 0;
+    }
+
+
+  }
+  function engineEfficiency(){
+    if (e == 4){
+      return 1;
+    }
+    else if(e == 6){
+      return 1.20;
+    }
+    else if(e == 8){
+      return 1.36;
+    }
+  }
+  return totalMiles, fuelLeft, fill, drive;
+}
+
+module.exports = Vehicle;
